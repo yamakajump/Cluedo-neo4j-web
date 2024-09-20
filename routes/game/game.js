b42d598e9025b1e009cadcf12d6a3cbf2b13f9df
yamakajump <corentin.batard2003@gmail.com>
2024-09-20 14:09:24 +0200

    redirection vers /game

--- routes/game/game.js
+++ routes/game/game.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-    
+    res.render('game', { title: 'Game' });
 });
 
 router.post('/', async function(req, res, next) {
-    
+
 });
 
 module.exports = router;
